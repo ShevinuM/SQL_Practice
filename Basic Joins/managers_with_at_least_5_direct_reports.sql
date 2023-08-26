38bfb9035f833c3b7cb92f525f47483373992930
ShevinuM <nsmcooray@mun.ca>
2023-08-26 16:57:00 +0530

    Create managers_with_at_least_5_direct_reports.sql

---
+++ Basic Joins/managers_with_at_least_5_direct_reports.sql
@@ -0,0 +1,48 @@
+/*
+Problem Description:
+
+Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+
+Write a solution to find managers with at least five direct reports.
+Return the result table in any order.
+
+Example 1:
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | None      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+*/
+
+SELECT table1.name
+FROM Employee AS table1
+INNER JOIN Employee AS table2
+ON table2.managerId = table1.id
+GROUP BY table2.managerId
+HAVING COUNT(table2.managerId) >= 5;
